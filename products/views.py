8a89ed0f7b0de15389de88891fc3577f036da2ea
tjdanielson <tessajdanielson@gmail.com>
2022-03-15 11:08:19 -0500

    Created logic for updating a product in the get_product_by_id view/function.

--- products/views.py
+++ products/views.py
@@ -1,8 +1,5 @@
-from urllib import response
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
-
-import products
 from .serializers import ProductSerializer, serializers
 from .models import Product
 from rest_framework.response import Response
@@ -22,10 +19,16 @@ def products_list(request):
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-@api_view(['GET'])
+@api_view(['GET', 'PUT'])
 def products_by_id(request, pk):
     product = get_object_or_404(Product, pk=pk)
     if request.method == 'GET':
         serializer = ProductSerializer(product)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
 
