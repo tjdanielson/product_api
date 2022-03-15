6b6defb1dfcf475e67a59b2cf1285a516eb10f1f
tjdanielson <tessajdanielson@gmail.com>
2022-03-15 13:32:51 -0500

    Created model for review class.

--- reviews/models.py
+++ reviews/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from products.models import Product
 
 # Create your models here.
+class Review(models.Model):
+    username = models.CharField(max_length=255)
+    description = models.CharField(max_length=1000)
+    stars = models.IntegerField(max_digits=1)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
