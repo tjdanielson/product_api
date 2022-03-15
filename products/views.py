c10d8935f9aab102259620d6dd19aad9e4767d0f
tjdanielson <tessajdanielson@gmail.com>
2022-03-15 16:34:37 -0500

    Refactor post/create a product view from function-based to class in ProductList class.

--- products/views.py
+++ products/views.py
@@ -13,9 +13,14 @@ class ProductList(APIView):
         products = Product.objects.all()
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
-        
-
+    
+    def post(self, request):
+        serializer = ProductSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 #Function based views:
