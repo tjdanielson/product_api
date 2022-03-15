f2da847036bc191c92a58893a64eeebcdc52f0ae
tjdanielson <tessajdanielson@gmail.com>
2022-03-15 11:22:48 -0500

    Added an image field to the model and migrated that update to the database.

--- products/models.py
+++ products/models.py
@@ -6,3 +6,4 @@ class Product(models.Model):
     description = models.CharField(max_length=500)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory_quantity = models.IntegerField()
+    image = models.URLField(default="https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?20200913095930")
--- products/views.py
+++ products/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import get_object_or_404
 from rest_framework.decorators import api_view
-from .serializers import ProductSerializer, serializers
+from .serializers import ProductSerializer
 from .models import Product
 from rest_framework.response import Response
 from rest_framework import status
