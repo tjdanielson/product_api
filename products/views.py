0b0e9e7ff019caef0d0a880b825c87d3ead0ef46
tjdanielson <tessajdanielson@gmail.com>
2022-03-15 10:41:48 -0500

    Created start of view/url for getting products by id, created view to post a new product in product list view.

--- products/views.py
+++ products/views.py
@@ -3,11 +3,22 @@ from rest_framework.decorators import api_view
 from .serializers import ProductSerializer, serializers
 from .models import Product
 from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def products_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET'])
+def products_by_id(request, pk):
+    pass
