9d87bd11c905bc7c0d00b686551d92d56dcc5e60
tjdanielson <tessajdanielson@gmail.com>
2022-03-15 10:22:23 -0500

    Added view to get all products, updated URLs with a url to get products view for all products.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from urllib import response
+from rest_framework.decorators import api_view
+from .serializers import ProductSerializer, serializers
+from .models import Product
+from rest_framework.response import Response
 
 # Create your views here.
+
+@api_view(['GET'])
+def products_list(request):
+    products = Product.objects.all()
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
